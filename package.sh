4a57f0d3074892826d2f4f33a2e16c372e6ce223
Steve Taggart <11730266+stevetaggart@users.noreply.github.com>
2025-10-08 11:30:52 -0600

    Update certbot version; add GH actions build

--- package.sh
+++ package.sh
@@ -5,7 +5,7 @@ set -e
 readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 readonly CERTBOT_VERSION=$( awk -F= '$1 == "certbot"{ print $NF; }' "${SCRIPT_DIR}/requirements.txt" )
 readonly VENV="certbot/venv"
-readonly PYTHON="python3"
+readonly PYTHON="python"
 readonly CERTBOT_ZIP_FILE="certbot.zip"
 readonly CERTBOT_SITE_PACKAGES=${VENV}/Lib/site-packages
 
@@ -14,10 +14,10 @@ cd "${SCRIPT_DIR}"
 ${PYTHON} -m venv "${VENV}"
 source "${VENV}/Scripts/activate"
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 pushd ${CERTBOT_SITE_PACKAGES}
-    zip -r -q ${SCRIPT_DIR}/certbot/${CERTBOT_ZIP_FILE} . -x "/*__pycache__/*"
+    7z a -tzip ${SCRIPT_DIR}/certbot/${CERTBOT_ZIP_FILE} . -xr!__pycache__
 popd
 
-zip -g "certbot/${CERTBOT_ZIP_FILE}" main.py
+7z a -tzip "certbot/${CERTBOT_ZIP_FILE}" main.py
